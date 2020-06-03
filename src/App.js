ca3ee9429b8675ad893b6932a0748886a8c454f2
shoaib muneer <shoaib76muneer@gmail.com>
2020-06-03 13:28:45 +0500

    react first edit by shoaib delete  button

--- src/App.js
+++ src/App.js
@@ -7,12 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <CustomButton
-  green={true}
-  width={64}
-  options={{ awesome: "yes", disabled: "no" }}
-  onClick={doStuffFunc}
-/>
+        
         <p>
           Edit <code>src/App.js</code> and save to reload.
           <br/>
